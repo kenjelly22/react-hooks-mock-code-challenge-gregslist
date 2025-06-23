15ed379b23765e4bb082377c6271bd1c5f9db86d
Jen Kelly <jen.kelly@powerhrg.com>
2025-06-23 09:34:42 -0400

    Call fetch request with useEffect to run on page load

--- src/components/ListingsContainer.js
+++ src/components/ListingsContainer.js
@@ -1,14 +1,19 @@
-import React from "react";
+import React, {useEffect} from "react"
 // import ListingCard from "./ListingCard";
 
 function ListingsContainer() {
+  useEffect(() => {
+    fetch("http://localhost:6001/listings")
+      .then((r) => r.json())
+      .then((listings) => console.log(listings))
+  })
   return (
     <main>
       <ul className="cards">
         {/* use the ListingCard component to display listings */}
       </ul>
     </main>
-  );
+  )
 }
 
-export default ListingsContainer;
+export default ListingsContainer
